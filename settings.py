eaad83802c6f680950c4efe93c35f5e1bfce8f72
rafael_rotiroti <rafael.rotiroti@mercadobitcoin.com>
2021-10-11 16:07:21 -0300

    function that creates a files path

--- settings.py
+++ settings.py
@@ -3,3 +3,7 @@ import os
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 FILE_PATH = 'files'
 URL_PATH = f'{BASE_PATH}/{FILE_PATH}'
+
+def create_files_path():
+    files_path = os.path.join(BASE_PATH, FILE_PATH)
+    os.mkdir(files_path)
